0bdaf877a88e7a32d01d412fec1fab6b7bacc9fa
David <davidcoates95@gmail.com>
2026-08-20 22:06:26 +1000

    unbind awkward thumb keys & swap window / mouse

--- keyboards/keebio/nyquist/rev5/keymaps/davidcoates/keymap.c
+++ keyboards/keebio/nyquist/rev5/keymaps/davidcoates/keymap.c
@@ -1,11 +1,7 @@
 #include QMK_KEYBOARD_H
 
-enum planck_keycodes {
-  ST_MACRO_0 = SAFE_RANGE,
-  ST_MACRO_1,
-};
-
 #define KC_SCR_SHOT LCTL(LSFT(KC_PSCR))
+#define KC_WIN_SEL LALT(LCTL(KC_DOWN))
 
 void keyboard_post_init_user(void) {
   set_unicode_input_mode(UNICODE_MODE_LINUX);
@@ -17,111 +13,149 @@ void keyboard_post_init_user(void) {
 
 enum unicode_names {
   EN_DASH,
-  EM_DASH,
+};
+
+enum custom_keycodes {
+  SYM_CTRL = SAFE_RANGE,
 };
 
 const uint32_t unicode_map[] PROGMEM = {
   [EN_DASH]   = 0x2013, // –
-  [EM_DASH]   = 0x2014, // —
 };
 
 enum planck_layers {
   _BASE,
-  _QWERTY,
   _SYMBOL,
   _NUMPAD,
-  _CONTROL,
+  _FUNCTION,
   _ARROW,
   _MOUSE,
-  _FUNCTION,
   _WINDOW,
+  _CONTROL,
+  _QWERTY,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_ortho_4x12(
-    KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_J,           KC_L,           KC_U,           KC_Y,           KC_QUOTE,       KC_DELETE,
-    KC_MINUS,       KC_A,           KC_R,           KC_S,           KC_T,           KC_G,           KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           KC_SCLN,
-    KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       MO(_ARROW),
-    KC_ESCAPE,      TO(_QWERTY),    KC_LEFT_ALT,    KC_LEFT_CTRL,   OSL(_SYMBOL),   KC_SPACE,       KC_BSPC,        OSL(_NUMPAD),   KC_LEFT_GUI,    ST_MACRO_0,     ST_MACRO_1,     KC_ENTER
-  ),
 
-  [_QWERTY] = LAYOUT_ortho_4x12(
-    KC_TRANSPARENT, KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, TO(_BASE),      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+  [_BASE] = LAYOUT_ortho_4x12(
+    KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,          KC_J,           KC_L,           KC_U,           KC_Y,           KC_QUOTE,       KC_DELETE,
+    KC_MINUS,       KC_A,           KC_R,           KC_S,           KC_T,           KC_G,          KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           KC_SCLN,
+    OSM(MOD_LSFT),  KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,          KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       MO(_ARROW),
+    KC_ESCAPE,      KC_NO,          KC_NO,          KC_LEFT_CTRL,   SYM_CTRL,       KC_SPACE,      KC_BSPC,        OSL(_NUMPAD),   KC_LEFT_ALT,    KC_NO,          KC_NO,          KC_ENTER
   ),
 
   [_SYMBOL] = LAYOUT_ortho_4x12(
-    KC_SCR_SHOT,    KC_GRAVE,       UC(EN_DASH),    KC_PIPE,        KC_ASTR,        KC_TRANSPARENT, KC_TRANSPARENT, KC_BSLS,        KC_CIRC,        UC(EM_DASH),    KC_DQUO,        KC_TRANSPARENT,
-    KC_UNDS,        KC_AT,          KC_AMPR,        KC_LCBR,        KC_RCBR,        KC_LBRC,        KC_RBRC,        KC_LPRN,        KC_RPRN,        KC_EQUAL,       KC_PLUS,        KC_COLN,
-    KC_TRANSPARENT, KC_TILD,        KC_EXLM,        KC_PERC,        KC_DLR,         KC_TRANSPARENT, KC_TRANSPARENT, KC_HASH,        KC_LABK,        KC_RABK,        KC_QUES,        KC_TRANSPARENT,
-    KC_TRANSPARENT, TO(_BASE),      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, MO(_CONTROL),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_NO,          KC_GRAVE,       UC(EN_DASH),    KC_PIPE,        KC_ASTR,        KC_NO,         KC_NO,          KC_BSLS,        KC_CIRC,        KC_NO,          KC_DQUO,        KC_NO,
+    KC_UNDS,        KC_AT,          KC_AMPR,        KC_LCBR,        KC_RCBR,        KC_LBRC,       KC_RBRC,        KC_LPRN,        KC_RPRN,        KC_EQUAL,       KC_PLUS,        KC_COLN,
+    KC_NO,          KC_TILD,        KC_EXLM,        KC_PERC,        KC_DLR,         KC_NO,         KC_NO,          KC_HASH,        KC_LABK,        KC_RABK,        KC_QUES,        KC_NO,
+    KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS,        KC_TRNS,        KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS
   ),
 
   [_NUMPAD] = LAYOUT_ortho_4x12(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_4,           KC_5,           KC_6,           KC_0,           KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_DOT,         MO(_FUNCTION),
-    KC_TRANSPARENT, TO(_BASE),      KC_TRANSPARENT, KC_TRANSPARENT, MO(_CONTROL),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
-  ),
-
-  [_CONTROL] = LAYOUT_ortho_4x12(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,      KC_AUDIO_VOL_UP,   KC_MEDIA_PLAY_PAUSE, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, QK_BOOT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK, KC_AUDIO_VOL_DOWN, KC_MEDIA_NEXT_TRACK, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,      KC_TRANSPARENT,    KC_TRANSPARENT,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, TO(_BASE),      KC_TRANSPARENT,      KC_TRANSPARENT,    KC_TRANSPARENT,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
-  ),
-
-  [_ARROW] = LAYOUT_ortho_4x12(
-    KC_TRANSPARENT, KC_PAGE_UP,     KC_HOME,        KC_UP,          KC_END,         KC_PLUS,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_PAGE_DOWN,   KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_MINUS,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, TO(_BASE),      KC_TRANSPARENT, KC_TRANSPARENT, MO(_WINDOW),    KC_TRANSPARENT, KC_TRANSPARENT, MO(_MOUSE),     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
-  ),
-
-  [_MOUSE] = LAYOUT_ortho_4x12(
-    KC_TRANSPARENT, QK_MOUSE_WHEEL_UP,   QK_MOUSE_BUTTON_2,    QK_MOUSE_CURSOR_UP,   QK_MOUSE_BUTTON_1,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, QK_MOUSE_WHEEL_DOWN, QK_MOUSE_CURSOR_LEFT, QK_MOUSE_CURSOR_DOWN, QK_MOUSE_CURSOR_RIGHT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT,      KC_TRANSPARENT,       KC_TRANSPARENT,       KC_TRANSPARENT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, TO(_BASE),           KC_TRANSPARENT,       KC_TRANSPARENT,       KC_TRANSPARENT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_NO,          KC_NO,          KC_NO,          MO(_FUNCTION),  KC_NO,          KC_NO,         KC_NO,          KC_7,           KC_8,           KC_9,           KC_NO,          KC_NO,
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO,          KC_4,           KC_5,           KC_6,           KC_0,           KC_NO,
+    KC_LEFT_SHIFT,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO,          KC_1,           KC_2,           KC_3,           KC_DOT,         KC_NO,
+    KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS,        KC_NO,          KC_NO,         KC_NO,          KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_TRNS
   ),
 
   [_FUNCTION] = LAYOUT_ortho_4x12(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F4,          KC_F5,          KC_F6,          KC_F11,         KC_TRANSPARENT,
-    KC_CAPS_LOCK,   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F12,         KC_TRANSPARENT,
-    KC_TRANSPARENT, TO(_BASE),      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_NO,          KC_NO,          KC_NO,          KC_TRNS,        KC_NO,          KC_NO,         KC_NO,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_NO,
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO,          KC_F4,          KC_F5,          KC_F6,          KC_F11,         KC_NO,
+    KC_CAPS_LOCK,   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO,          KC_F1,          KC_F2,          KC_F3,          KC_F12,         KC_NO,
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO
+  ),
+
+  [_ARROW] = LAYOUT_ortho_4x12(
+    KC_NO,          KC_PAGE_UP,     KC_HOME,        KC_UP,          KC_END,         KC_PLUS,       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          KC_PAGE_DOWN,   KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_MINUS,      KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_LEFT_SHIFT,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS,
+    KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS,        KC_NO,          KC_NO,         KC_NO,          MO(_WINDOW),    MO(_MOUSE),     KC_NO,          KC_NO,          KC_TRNS
+  ),
+
+  [_MOUSE] = LAYOUT_ortho_4x12(
+    KC_NO,          MS_WHLU,        MS_BTN2,        MS_UP,          MS_BTN1,        KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          MS_WHLD,        MS_LEFT,        MS_DOWN,        MS_RGHT,        KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS,        KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS
   ),
 
   [_WINDOW] = LAYOUT_ortho_4x12(
-    KC_TRANSPARENT, KC_TRANSPARENT, LALT(LCTL(KC_UP)), LGUI(KC_UP),    LALT(LCTL(KC_DOWN)), KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_LEFT),     LGUI(KC_DOWN),  LGUI(KC_RIGHT),      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,    KC_TRANSPARENT, KC_TRANSPARENT,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, TO(_BASE),      KC_TRANSPARENT,    KC_TRANSPARENT, KC_TRANSPARENT,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_NO,          KC_NO,          LCTL(KC_DOWN),   KC_TAB,        LCTL(KC_UP),    KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          KC_NO,          LCTL(KC_LEFT),   LSFT(KC_TAB),  LCTL(KC_RIGHT), KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_TRNS,        KC_NO,          KC_NO,           KC_NO,         KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_TRNS,        KC_NO,          KC_NO,           KC_TRNS,       KC_NO,          KC_NO,         KC_NO,          KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_TRNS
+  ),
+
+  [_CONTROL] = LAYOUT_ortho_4x12(
+    KC_NO,          TO(_QWERTY),    KC_WIN_SEL,      KC_VOLU,       KC_MPLY,        QK_BOOT,       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          KC_SCR_SHOT,    KC_MPRV,         KC_VOLD,       KC_MNXT,        KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          KC_NO,          KC_NO,           KC_NO,         KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          KC_NO,          KC_NO,           KC_NO,         KC_TRNS,        KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO
+  ),
+
+  [_QWERTY] = LAYOUT_ortho_4x12(
+    TO(_BASE),      KC_Q,           KC_W,           KC_E,           KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          KC_A,           KC_S,           KC_D,           KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_NO,          KC_NO,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_ESCAPE,      KC_NO,          KC_NO,          KC_LEFT_CTRL,   KC_NO,          KC_SPACE,      KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO
   ),
 
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case ST_MACRO_0:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_TAP(X_LEFT))));
-    }
-    break;
-    case ST_MACRO_1:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_TAP(X_RIGHT))));
-    }
-    break;
+layer_state_t layer_state_set_user(layer_state_t state) {
+  clear_oneshot_mods();
+  if (layer_state_cmp(state, _WINDOW)) {
+    register_code(KC_LALT);
+  } else {
+    unregister_code(KC_LALT);
   }
-  static int one_shot_layer = -1;
-  if ((keycode & QK_ONE_SHOT_LAYER) == QK_ONE_SHOT_LAYER) {
+  return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static int  one_shot_layer      = -1;
+  static bool sym_ctrl_symbol_held = false;
+  static bool sym_ctrl_symbol_used = false;
+
+  switch (keycode) {
+    case SYM_CTRL:
+      if (record->event.pressed) {
+        if (one_shot_layer != -1) {
+          layer_off(one_shot_layer);
+          one_shot_layer = -1;
+        }
+        if (get_mods() & MOD_MASK_SHIFT) {
+          layer_on(_CONTROL);
+          unregister_mods(MOD_MASK_SHIFT);
+        } else {
+          sym_ctrl_symbol_held = true;
+          sym_ctrl_symbol_used = false;
+          layer_on(_SYMBOL);
+        }
+      } else {
+        if (IS_LAYER_ON(_CONTROL)) {
+          layer_off(_CONTROL);
+        } else if (sym_ctrl_symbol_held) {
+          sym_ctrl_symbol_held = false;
+          if (sym_ctrl_symbol_used) {
+            layer_off(_SYMBOL);
+          } else {
+            one_shot_layer = _SYMBOL;
+          }
+        }
+      }
+      return false;
+  }
+
+  if (sym_ctrl_symbol_held && record->event.pressed) {
+    sym_ctrl_symbol_used = true;
+  }
+
+  if (IS_QK_ONE_SHOT_LAYER(keycode)) {
     if (record->event.pressed) {
       one_shot_layer = QK_ONE_SHOT_LAYER_GET_LAYER(keycode);
     }
-  } else if (one_shot_layer != -1) {
+  } else if (one_shot_layer != -1 && !sym_ctrl_symbol_held) {
     bool delay_unregister = IS_QK_BASIC(keycode) || IS_QK_MODS(keycode);
     if (record->event.pressed) {
       if (delay_unregister) {
@@ -129,7 +163,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
       }
     } else {
+      int layer_to_close = one_shot_layer;
       one_shot_layer = -1;
+      if (layer_to_close == _SYMBOL) {
+        layer_off(_SYMBOL);
+      }
       if (delay_unregister) {
         unregister_code16(keycode);
         return false;
@@ -162,11 +200,8 @@ bool rgb_matrix_indicators_user(void) {
     case _NUMPAD:
       highlight_key(6, 1);
       break;
-    case _CONTROL:
-      highlight_key(2, 3);
-      break;
-    case _QWERTY:
-      highlight_key(0, 1);
+    case _FUNCTION:
+      highlight_key(0, 3);
       break;
     case _ARROW:
       highlight_key(1, 1);
@@ -174,11 +209,14 @@ bool rgb_matrix_indicators_user(void) {
     case _MOUSE:
       highlight_key(6, 0);
       break;
-    case _FUNCTION:
-      highlight_key(0, 3);
-      break;
     case _WINDOW:
       highlight_key(0, 2);
+      break;
+    case _CONTROL:
+      highlight_key(2, 3);
+      break;
+    case _QWERTY:
+      highlight_key(0, 1);
       break;
   }
 
